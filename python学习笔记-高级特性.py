f8010275ed4f015eabeaa055b3258b15a8c9a3fe
dfw <farrell_Tung@hotmail.com>
2020-05-24 23:16:13 +0800

    commit 迭代 On dev

--- python学习笔记-高级特性.py
+++ python学习笔记-高级特性.py
@@ -19,3 +19,34 @@ def trime(s):
 	while s[-1:]=='':
 		s=s[:-1]
 	return s
+
+
+#迭代：通过for循环来遍历这个list或tuple，这种遍历我们称为迭代（Iteration）
+#默认情况下，dict迭代的是key。如果要迭代value，可以用for value in d.values()，
+#如果要同时迭代key和value，可以用for k, v in d.items()
+
+#通过collections模块的Iterable类型判断对象是否为可迭代对象
+from collections import Iterable
+isinstance('abc',Iterable)
+for x, y in [(1, 1), (2, 4), (3, 9)]:
+    print(x, y)
+'''输出
+1 1
+2 4
+3 9
+'''
+#练习：
+def FindMinAndMax(L):
+	if len(L)==0:
+        return (None,None)
+    else:
+        max=L[0]
+        min=L[0]
+        for i in L:
+            if i>max:
+                max=i
+            if i<min:
+                min=i
+        return(min,max)
+
+
