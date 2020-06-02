0aeb5742c11d120cd7a727509e147f5b3d3be5b3
dfw <farrell_Tung@hotmail.com>
2020-06-02 23:36:53 +0800

    提交

--- python学习笔记-高阶函数.py
+++ python学习笔记-高阶函数.py
@@ -101,3 +101,52 @@ def createCounter():
 f = lambda x:x*x
 
 
+#装饰器：要增强now()函数的功能，比如，在函数调用前后自动打印日志，
+# 但又不希望修改now()函数的定义，这种在代码运行期间动态增加功能的方式，
+# 称之为“装饰器”（Decorator）
+
+def now():
+    print('2020-06-01')
+
+#借助Python的@语法，把decorator置于函数的定义处
+def log(func):
+    def wrapper(*args,**kw):
+        print('call %s():',%func.__name__)
+        return func(*args,**kw)
+    return wrapper
+
+@log
+def new_now():
+    print('2020-06-02')
+
+'''
+>>> now()
+call now():
+2015-3-25
+把@log放到now()函数的定义处，相当于执行了语句：now = log(now)
+由于log()是一个decorator，返回一个函数，
+所以，原来的now()函数仍然存在，只是现在同名的now变量指向了新的函数，
+于是调用now()将执行新函数，即在log()函数中返回的wrapper()函数
+'''
+
+#如果decorator本身需要传入参数，
+# 那就需要编写一个返回decorator的高阶函数，写出来会更复杂。比如，要自定义log的文本：
+
+def log_text(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print('%s %s():' %(text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+#高阶decorator使用方式
+@log_text('excute')
+def now_new():
+    print('2020-06-02')
+
+f = now_new
+f()
+#和两层嵌套的decorator相比，3层嵌套的效果是这样的：>>> now = log('execute')(now)
+
+
